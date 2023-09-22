11f6f6bc6659d43f7889720e22fabf0b0de6d1ce
usmanim00 <145802524+usmanim00@users.noreply.github.com>
2023-09-22 22:15:55 +0300

    Create enteresan_sozluk.py

---
+++ enteresan_sozluk.py
@@ -0,0 +1,13 @@
+meme_dict ={
+    "LOL" : "komik bir şeye verilen cevap",
+    "CRINGE":"garip ya da utandırıcı bir şey",
+    "ROFL" : "bir şakaya karşılık cevap",
+    "SHEESH ": "onaylamamak",
+    "CREEPY" : "korkunç",
+    "AGGRO" : "agresifleşmek/sinirlenmek"
+}
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("aradığıız kelime suanda bulunamıyor")
